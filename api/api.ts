d0279b4261d2eed8e49f48ec079651b6c5f5488c
Jack Kitto <jack.kitto@gmail.com>
2025-08-03 11:32:28 +0900

    feat: create sim util type

--- api/api.ts
+++ api/api.ts
@@ -6,3 +6,4 @@ export const fetchClient = createFetchClient<paths>({
   baseUrl: "https://backend-134243472228.asia-east1.run.app",
 });
 export const $api = createClient(fetchClient);
+export type Sim = NonNullable<NonNullable<paths['/complete-order']['post']['responses']['200']['content']['application/json']['sims']>[number]>
