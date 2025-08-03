828d99133daaa879e7c45c583eda7df3369dcc46
Jack Kitto <jack.kitto@gmail.com>
2025-08-03 17:53:02 +0900

    feat: create add sim screen

--- app/checkoutStack/_layout.tsx
+++ app/checkoutStack/_layout.tsx
@@ -11,6 +11,7 @@ export default function Layout() {
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="checkout" options={{ headerShown: false }} />
+      <Stack.Screen name="addSim" options={{ headerShown: false }} />
     </Stack>
   )
 }
---
+++ app/checkoutStack/addSim.tsx
@@ -0,0 +1,6 @@
+import { AddSim } from "@/components/esimHome/components/addSim";
+
+export default function AddSimScreen() {
+  return <AddSim />
+}
+
