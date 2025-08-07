a5e8c109622dc22e5419909f94f0fd1aa8ce7008
Jack Kitto <jack.kitto@gmail.com>
2025-08-07 18:51:22 +0900

    chore: remove unused code

---
+++ components/esimHome/components/worldMapSvg/WorldMapSvg.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyleSheet, Dimensions } from "react-native";
+import Svg, { Path, Defs, LinearGradient, Stop } from "react-native-svg";
+import { path } from "./path";
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
+
+export const WorldMapSvg: React.FC = () => {
+  return (
+    <Svg
+      style={StyleSheet.absoluteFillObject}
+      width={screenWidth}
+      height={screenHeight}
+      viewBox="-200 60 891 404"
+      preserveAspectRatio="xMidYMid slice"
+      fill="none"
+    >
+      <Path
+        d={path}
+        fill="url(#paint0_linear_3734_9805)"
+        fillOpacity={0.13}
+      />
+      <Defs>
+        <LinearGradient
+          id="paint0_linear_3734_9805"
+          x1="445.171"
+          y1="0"
+          x2="445.171"
+          y2="404"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stopColor="#32D583" stopOpacity="0" />
+          <Stop offset="0" stopColor="#32D583" stopOpacity="0.7" />
+          <Stop offset="1" stopColor="#32D583" stopOpacity="0" />
+        </LinearGradient>
+      </Defs>
+    </Svg>
+  );
+};
