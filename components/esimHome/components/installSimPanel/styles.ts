ddc90421f4de4a500088832ab9adb932f037f274
Jack Kitto <jack.kitto@gmail.com>
2025-08-08 16:37:26 +0900

    feat: create esim panel

---
+++ components/esimHome/components/installSimPanel/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const $styles = StyleSheet.create({
+  root: {
+    backgroundColor: "#202939",
+    borderRadius: 24,
+    padding: 24,
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: "center",
+    minHeight: 400,
+  },
+  topUpButton: {
+    borderWidth: 2,
+    borderColor: "#32D583",
+    borderRadius: 25,
+    paddingHorizontal: 32,
+    paddingVertical: 12,
+    backgroundColor: "transparent",
+  },
+  installButtonText: {
+    color: "#32D583",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
