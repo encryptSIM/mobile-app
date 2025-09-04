f8be5b9a06a743b383f13373920d7f729c693f81
Jack Kitto <jack.kitto@gmail.com>
2025-09-05 08:19:34 +0900

    chore: remove unecessary aliases

--- metro.config.js
+++ metro.config.js
@@ -16,18 +16,12 @@ config.resolver.alias = {
   ...config.resolver.alias,
   ws: false,
   fs: false,
-  crypto: "react-native-crypto",
-  stream: "readable-stream",
-  buffer: "@craftzdog/react-native-buffer",
 };
 
 config.resolver.fallback = {
   ...config.resolver.fallback,
   ws: false,
   fs: false,
-  crypto: require.resolve("react-native-crypto"),
-  stream: require.resolve("readable-stream"),
-  buffer: require.resolve("@craftzdog/react-native-buffer"),
 };
 
 module.exports = withNativeWind(config, { input: "./global.css" });
