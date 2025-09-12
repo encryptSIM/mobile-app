100e4c5ecbd4dd7c372f3b4e61c86f095e8d107d
Jack Kitto <jack.kitto@gmail.com>
2025-09-12 13:00:41 +0900

    feat: add function to convrt sol to lamport

--- utils/lamports-to-sol.ts
+++ utils/lamports-to-sol.ts
@@ -3,3 +3,8 @@ import { LAMPORTS_PER_SOL } from '@solana/web3.js'
 export function lamportsToSol(balance: number) {
   return Math.round((balance / LAMPORTS_PER_SOL) * 100000) / 100000
 }
+
+export function solToLamports(solAmount: number): number {
+  return Math.round(solAmount * LAMPORTS_PER_SOL)
+}
+
